322009c2e153f75e06e3827a50c5edc90b764910
Moritz Messner <44976678+MessnerM@users.noreply.github.com>
2021-04-19 13:15:21 +0200

    fix

--- forum_alpha/controllers/post.js
+++ forum_alpha/controllers/post.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const samplePost = require('../data/samplePost.json');
 const sql = require('../db.js');
-/* GET home page. */
+
 router.get('/', function (req, res, next) {
     res.render("post", samplePost)
 });
